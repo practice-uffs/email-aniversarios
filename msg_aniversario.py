54f5de702da3dd5c2669eedb8cfd6c867b78cfae
Gian <giancds@gmail.com>
2023-04-04 19:45:01 -0300

    adicionada utilização da biblioteca 'envolope' para envio dos emails com imagem em anexo no corpo da mensagem

--- msg_aniversario.py
+++ msg_aniversario.py
@@ -1,7 +1,5 @@
 import pandas as pd
-from email.message import EmailMessage
-from email.utils import make_msgid
-import smtplib
+from envelope import Envelope
 
 from dotenv import dotenv_values
 config = dotenv_values(".env")
@@ -12,14 +10,11 @@ aniversariantes = 0
 url = f"https://docs.google.com/spreadsheets/d/{config['DATASHEET_ID']}/gviz/tq?tqx=out:csv&sheet={config['DATASHEET_NAME']}"
 
 # dados vem das variáveis de ambiente
+server_host = config['MAIL_SERVER']
+server_port = config['MAIL_PORT']
 sender_name = config['MAIL_NAME']
 sender_email = config['MAIL_USER']
 sender_password = config['MAIL_PASS']
-mail_from = sender_name + ' <' + sender_email + '>'
-
-# servidor, porta e tipo de autenticação
-server = smtplib.SMTP(config['MAIL_SERVER'], config['MAIL_PORT'])
-server.starttls
 
 # carrega data de hoje para verificar aniversariantes do dia
 dia_de_hoje = pd.to_datetime('today').date().strftime('%d/%m')
@@ -39,37 +34,40 @@ aniversariantes = df.query(f"(Ativo == 'sim') & (Nascimento == '{dia_de_hoje}')"
 
 # se houverem aniversariantes envia a mensagem, senão imprime mensagem informativa
 if len(aniversariantes) > 0:
-    server.login(sender_email, sender_password)
+    # servidor, porta e tipo de autenticação
+    server = Envelope(
+            from_=sender_email
+        ).smtp(
+            host=server_host,
+            port=server_port,
+            user=sender_email,
+            password=sender_password,
+            security='starttls')
     for i, row in aniversariantes.iterrows():
         print(row)
 
-        message = EmailMessage()
         if row['Prefixo'] == 'nao':
             msg = row['Nome']
         else:
             msg = f"{row['Prefixo']}  {row['Nome']}"
-        message['Subject'] = 'Feliz Aniversário'
-        message['From'] = mail_from
-        message['To'] = row['email']
-        asparagus_cid = make_msgid()
-        message.set_content(row['Nome_Completo'])
 
-        message.add_alternative(f"""
-        <html>
-          <head></head>
-          <body>
-            <h4>Feliz Aniversário
-            {str(msg)}
-            !!!</h4>
-            <img src="cid:{asparagus_cid[1:-1]}" />
-          </body>
-        </html>
-        """, subtype='html')
-        message.get_payload()[1].add_related(img_data, 'image', 'png', cid=asparagus_cid)
+        html_message = f"""
+                <html>
+                <head></head>
+                <body>
+                    <h4>Feliz Aniversário {str(msg)}!</h4>
+                    <img src="cid:niver.png" />
+                </body>
+                </html>
+                """
+        server.attach(
+                path="niver.png",
+                inline=True
+            ).subject("Feliz Aniversário!").message(html_message).to(row['email']).send()
 
-
-        server.send_message(message)
         print(msg + ': ' + row['email'])
-    server.quit()
+
+    server.smtp_quit()
+
 else:
     print(' Sem aniversários hoje!')
